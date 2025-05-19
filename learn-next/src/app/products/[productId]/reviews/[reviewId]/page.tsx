53f80be6fd49661bd834d83e7f01ce2f8147cb71
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-05-19 12:59:02 +0700

    feat: not found page

--- learn-next/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ learn-next/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from "next/navigation";
+
 export default async function ReviewDetail({
     params,
 }:{
     params:Promise<{productId: string, reviewId: string}>;
 }) {
     const {productId, reviewId} = await params;
-    return <h1>Review {reviewId} for Product {productId}</h1>;
-
+    if(parseInt(reviewId) > 500){
+        notFound();
+    }
+    return (
+        <h1>Review {reviewId} for Product {productId}</h1>
+    );
 }
